7a3e9597c23bf0807b394c1ae9e8a7d2fff428c2
arnau <arnaubisqui70@gmail.com>
2021-02-15 18:17:09 +0100

    addSugerencias

--- resources/views/sugerencias.blade.php
+++ resources/views/sugerencias.blade.php
@@ -2,4 +2,22 @@
 
 @section('contenido')
     
+<form>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Tu correo</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Sugerencia</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Descripcion sugerencia</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+
+
+
 @endsection
